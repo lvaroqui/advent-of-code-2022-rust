4e05ca7c29b7a472040fd52c33de54e284ac7d5e
Luc Varoqui <luc@varoqui.org>
2022-12-03 22:24:27 +0100

    Use u64 instead of bitset

--- day03/src/lib.rs
+++ day03/src/lib.rs
@@ -1,36 +1,19 @@
-use std::ops::BitAnd;
-
 use common::DayResult;
-use fixedbitset::FixedBitSet;
 use itertools::Itertools;
-
-fn to_bitset(s: &str) -> FixedBitSet {
-    let mut res = FixedBitSet::with_capacity(53);
-    for b in s.as_bytes().iter() {
-        let b = match b {
-            b'a'..=b'z' => 1 + b - b'a',
-            b'A'..=b'Z' => 27 + b - b'A',
-            _ => unreachable!(),
-        };
-        res.insert(b.into())
-    }
-    res
-}
 
 pub struct Day3;
 
 impl common::DaySolver for Day3 {
     fn solve_1(&self, input: &str) -> DayResult {
-        let res: usize = input
+        input
             .split('\n')
             .map(|l| {
                 let (a, b) = l.split_at(l.len() / 2);
-                let (a, ref b) = (to_bitset(a), to_bitset(b));
-                a.bitand(b).ones().next().unwrap()
+                let (a, b) = (to_bitset(a), to_bitset(b));
+                Ones::new(a & b).next().unwrap()
             })
-            .sum();
-
-        res.into()
+            .sum::<u64>()
+            .into()
     }
 
     fn solve_2(&self, input: &str) -> u64 {
@@ -38,7 +21,47 @@ impl common::DaySolver for Day3 {
             .split('\n')
             .map(to_bitset)
             .tuples()
-            .map(|(a, ref b, ref c)| (a.bitand(b).bitand(c)).ones().next().unwrap() as u64)
+            .map(|(a, ref b, ref c)| Ones::new(a & b & c).next().unwrap())
             .sum()
+    }
+}
+
+fn to_bitset(s: &str) -> u64 {
+    let mut res = 0;
+    for b in s.as_bytes().iter() {
+        let b = match b {
+            b'a'..=b'z' => 1 + b - b'a',
+            b'A'..=b'Z' => 27 + b - b'A',
+            _ => unreachable!(),
+        };
+        res |= 1 << b;
+    }
+    res
+}
+
+struct Ones {
+    inner: u64,
+    index: u8,
+}
+
+impl Ones {
+    fn new(inner: u64) -> Self {
+        Self { inner, index: 0 }
+    }
+}
+
+impl Iterator for Ones {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.inner != 0 {
+            let v = self.inner & 1;
+            self.index += 1;
+            self.inner >>= 1;
+            if v == 1 {
+                return Some((self.index - 1).into());
+            }
+        }
+        None
     }
 }
